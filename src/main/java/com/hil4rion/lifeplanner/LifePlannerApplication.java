c2bb35e56824e61efbd355414ba000bb5f54758d
Hil4ri0n <hilarion1337@gmail.com>
2026-01-19 20:53:57 +0100

    initial commit

---
+++ src/main/java/com/hil4rion/lifeplanner/LifePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.hil4rion.lifeplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifePlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifePlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hil4rion/lifeplanner/LifePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hil4rion.lifeplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifePlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
